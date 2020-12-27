910ffb62ef3ee1d94432b025aca6d8c2fcb03dce
Kimbangg <ehehdgus1@hanyang.ac.kr>
2020-12-27 16:23:30 +0900

    Assign1

---
+++ KDonghyeon/11047.java
@@ -0,0 +1,32 @@
+package com.company;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("동전 종류의 개수와 원하는 가치의 합을 입력하시오.");
+        int n = scan.nextInt();
+        int m = scan.nextInt();
+        int []coin = new int[n];
+        int count = 0;
+
+        for ( int i = 0; i < n; i++) {
+            coin[i] = scan.nextInt();
+        }
+
+        for ( int i = n-1; i >=0; i--) {
+            if ( m == 0 ) {
+                break;
+            }
+            if ( coin[i] > m ) {
+                continue;
+            }
+            else {
+                count += m / coin[i];
+                m = m % coin[i];
+            }
+        }
+
+        System.out.println(count);
+    }
+}
