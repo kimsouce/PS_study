cf44a0f6c0eb98ac5c813175ce07edc8074930d1
Kimsouce <60845730+kimsouce@users.noreply.github.com>
2021-01-16 18:21:55 +0900

    Update 1012 유기농배추.py

--- Soyeon/05 dfs,bfs/1012 유기농배추.py
+++ Soyeon/05 dfs,bfs/1012 유기농배추.py
@@ -1,17 +1,5 @@
-
-
-
-'''
-#런타임에러.. 수정 필요
 import sys
-t = int(input())
-for _ in range(t):
-  n ,m ,k = list(map(int,sys.stdin.readline().split()))
-  field = [[0]*m for _ in range(n)]
-  for _ in range(k):
-    x,y =map(int, input().split())
-    field[x][y] = 1
-
+sys.setrecursionlimit(10000)  #런타임 에러 방지  #파이썬에서 재귀함수 쓸 때는 항상 limit 
 
 def dfs(x,y):
   if x<=-1 or x>=n or y<=-1 or y>=m:
@@ -24,12 +12,17 @@ def dfs(x,y):
     dfs(x,y+1)
     return True
   return False
-  
-count = 0
-for i in range(n):
-  for j in range(m):
-    if dfs(i,j) == True:
-      count +=1
 
-print(count) 
-'''
+t = int(input())
+for _ in range(t):
+  n ,m ,k = list(map(int, input().split()))
+  field = [[0]*m for _ in range(n)]
+  count = 0
+  for _ in range(k):
+    x,y =map(int, input().split())
+    field[x][y] = 1
+  for i in range(n):
+    for j in range(m):
+      if dfs(i,j) == True:
+        count +=1
+  print(count)
