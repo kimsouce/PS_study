0eee09ecab53612a648cb6542c392a506de5b1a4
Kimsouce <60845730+kimsouce@users.noreply.github.com>
2021-01-20 22:40:28 +0900

    Create 9205 맥주 마시면서 걸어가기.py

---
+++ Soyeon/06 정렬/9205 맥주 마시면서 걸어가기.py
@@ -0,0 +1,54 @@
+'''
+# 오류코드 수정필요
+
+
+from collections import deque
+
+dx = [0, 0, -1, 1]  #LRUD
+dy = [-1, 1, 0, 0] 
+
+
+def bfs(x, y):  #최단거리 찾기(맥주가 떨어지지 않도록 편의점을 들리면서 페스티벌에 도착할 수 있는가)
+  while q:
+    x, y = q.popleft()
+    for k in range(4):
+      nx = x + dx[k]
+      ny = y + dy[k]
+
+      if 0<=nx<f_x and 0<=ny<f_y and (map[nx][ny] ==1 or map[nx][ny] == 2) :
+        if nx % 20 != 0 or ny % 20 != 0:
+          map[nx][ny] = 3
+        else:
+          q.append((nx, ny))
+
+def mood():
+  for i in range(f_x):
+    for j in range(f_y):
+      tmp = map[i][j]
+      if tmp ==3:
+        print('sad')
+      else:
+        print('happy')
+
+
+t = int(input())
+for _ in range(t):
+  n = int(input())
+  h_x, h_y = list(map(int, input().split()))  #home의 x좌표 y좌표
+  for _ in range(n):
+    s_x, s_y = map(int, input().split())  #shop의 x좌표 y좌표
+  f_x, f_y = map(int, input().split())  #fetival의 x좌표 y좌표
+
+  map = [[0]*(f_y+1) for _ in range(f_x+1)]
+  map[s_x][s_y] = 1
+  map[f_x][f_y] = 2
+
+  q = deque()
+  for i in range(f_x):
+    for j in range(f_y):
+      if map[i][j] ==1 or map[i][j] ==2:
+        q.append((i, j))
+
+  bfs(0, 0)
+  mood()
+  '''
