22816fa050d9e807f0e3f390cd56d98b5d77504b
Jinjuseong <jinju4948@naver.com>
2021-01-23 23:05:53 +0900

    Sorting

---
+++ Juseong/Sorting/1157 단어 공부.py
@@ -0,0 +1,13 @@
+word = input().upper()
+word_list = list(set(word))
+word_cnt = []
+
+for i in word_list:
+    cnt = word.count(i)
+    word_cnt.append(cnt)
+
+if word_cnt.count((max(word_cnt))) >= 2:
+    print('?')
+else:
+    print(word_list[word_cnt.index(max(word_cnt))])
+
---
+++ Juseong/Sorting/1213 팰린드롬 만들기.py
@@ -0,0 +1,9 @@
+word = input()
+
+n = len(word)
+
+if n/2 == 0:
+    half = n/2
+else:
+    half = n // 2
+
