c3b48c5038ea06d66f43fdf4a686db28ea4f4ecc
Kimsouce <60845730+kimsouce@users.noreply.github.com>
2021-01-25 21:09:14 +0900

    Update 1920 수 찾기.py

--- Soyeon/07 이진탐색/1920 수 찾기.py
+++ Soyeon/07 이진탐색/1920 수 찾기.py
@@ -6,17 +6,17 @@ array_M = input().split()
 
 def binary_search(array, target, start, end):
   while start <= end:
-    mid = (start+end) //2
-    if array[mid] == target:
-      return mid
+    mid = (start+end) //2   
+    if array[mid] == target:  
+      return mid     #해당 정수 찾은 경우 인덱스 반환
     elif array[mid] < target:
-      start = mid+1
+      start = mid+1  #target보다 중간값이 작은 경우, 중간값 뒤 쪽만 확인하면 됨
     else:
-      end = mid -1
+      end = mid -1   #target보다 중간값이 큰 경우, 중간값 앞 쪽만 확인하면 됨
   return None
 
 for i in array_M:
-  result = binary_search(A, i, 0, n-1)
+  result = binary_search(A, i, 0, n-1)     #주어진 M개의 수 들이 리스트 A에 있는지 확인
   if result != None:
     print('1', sep='/n')
   else:
