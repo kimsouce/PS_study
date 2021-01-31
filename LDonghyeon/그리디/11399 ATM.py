c7dc5aaab44a1b7ec41a82b1c8b58831e14790c9
Kimsouce <60845730+kimsouce@users.noreply.github.com>
2021-01-31 18:24:26 +0900

    Delete LDonghyeon directory

--- LDonghyeon/그리디/11399 ATM.py
+++
@@ -1,9 +0,0 @@
-N = int(input())
-time = list(map(int,input().split()))
-
-time.sort()
-sum_arr = []
-for i in range(1, len(time)):
-    sum_arr.append(sum(time[:i])+time[i])
-
-print(sum(sum_arr) + time[0])
