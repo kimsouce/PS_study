d64b2ff32c271a281cb36ef529cf95bde4e6e829
Kimsouce <60845730+kimsouce@users.noreply.github.com>
2021-01-31 18:28:21 +0900

    Update 2343 기타 레슨.py

--- Soyeon/07 이진탐색/2343 기타 레슨.py
+++ Soyeon/07 이진탐색/2343 기타 레슨.py
@@ -1,3 +1,32 @@
+import sys
+input=sys.stdin.readline
+
+n,m=map(int,input().split())
+lesson=list(map(int,input().split()))
+# print(lesson)
+l=max(lesson)
+r=sum(lesson)
+ans=sys.maxsize
+while l<=r:
+    mid=(l+r)//2
+    cnt=0
+    sum=0
+    for i in range(len(lesson)):
+        if sum+lesson[i]>mid:
+            cnt+=1
+            sum=0
+        sum+=lesson[i]
+    if sum:
+        cnt+=1
+
+    if cnt>m: 
+        l=mid+1
+    else:
+        ans=min(ans,mid)
+        r=mid-1
+print(ans)
+
+
 '''
 #오류 코드
 n,m = map(int, input().split())
