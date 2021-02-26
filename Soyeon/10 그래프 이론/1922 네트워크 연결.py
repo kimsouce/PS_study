0b9a43fd3c05d08f51a9b5a61d4b5a364207a5df
Kimsouce <60845730+kimsouce@users.noreply.github.com>
2021-02-26 23:41:27 +0900

    Update 1922 네트워크 연결.py

--- Soyeon/10 그래프 이론/1922 네트워크 연결.py
+++ Soyeon/10 그래프 이론/1922 네트워크 연결.py
@@ -6,7 +6,7 @@ for _ in range(m):
     graph.append((a, b, c))     #a컴퓨터와 b컴퓨터가 연결되어 있다(a에서 b로 가는 경로가 존재한다)
 
 
-node = [0]     #노드를 한번 씩만 방문하기 위해 리스트 선언  #인덱스를 1부터 시작하기 위해 0을 미리 추가해줌
+node = [0]      #인덱스를 1부터 시작하기 위해 0을 미리 추가해줌
 graph = sorted(graph, key=lambda k :k[2])     #weight 기준으로 간선 정보 정렬
 
 for i in range(1, n+1):
@@ -14,13 +14,13 @@ for i in range(1, n+1):
 
 def find(s):     
     if s != node[s]:     #자기자신이 자기자신의 부모노드가 아니라면   #즉, 진입하는 간선이 존재한다면 
-        node[s] = find(node[s])     #경로 압축     
+        node[s] = find(node[s])     #해당 노드의 부모 노드를 찾아줌   
     return node[s]
 
 def union(u, v):
     root1 = find(u)
     root2 = find(v)
-    node[root2] = root1     #root2가 root1의 부모노드
+    node[root2] = root1     #root1이 root2의 부모노드
     
 E = 0     #간선의 개수가 V-1이 되면 while문 멈추기 위해
 cost = 0     #최소비용 구하기 위해
