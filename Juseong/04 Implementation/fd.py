0f4a958e47b795f29ebb3e543dd4ece7efe6a89a
juseong <jinju4948@naver.com>
2021-03-19 19:10:19 +0900

    정수 삼각형

---
+++ Juseong/04 Implementation/fd.py
@@ -0,0 +1,3 @@
+for i in range(5):
+    if i >= 3:
+        p
--- Juseong/08 DP/1932 정수 삼각형.py
+++ Juseong/08 DP/1932 정수 삼각형.py
@@ -13,7 +13,7 @@ for i in range(1, n):
         if j == 0:
             up_left = 0
         else:
-            up_left = dp[i-1][j-1]
+            up_left = dp[i-1][j-1] #계속 dp테이블을 갱신시킨다.
         #바로 위에서 내려오는 경우
         if j == i:
             up = 0
